676c86ec37c76d870dd4012d784d9d96183a77a5
nouramine878 <64599500+nouramine878@users.noreply.github.com>
2020-04-30 17:17:44 +0200

    commit test

---
+++ test/src/application/SampleController.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SampleController {
+	
+	@FXML
+	private void  bookAction (ActionEvent event) {
+		
+	}
+	
+	
+	}
+	
+
+ec
